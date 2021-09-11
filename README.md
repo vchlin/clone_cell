7417263edebddbbdd91eaaeaea41496f5e9ec68c
Vektorlynk <43309580+Vektorlynk@users.noreply.github.com>
2021-09-11 23:28:36 +0000

    Doc links and section rewrites

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 # clone_cell
 
 clone_cell provides a `Cell` implementation that works with types whose `Clone`
-implementations are guaranteed not to mutate the `Cell` content through the `self`
-reference the `clone` method gets. This is done with the provided `PureClone` trait,
-which is a subtrait of `Clone` (and a logical supertrait of `Copy`). It is only
-implemented for types with compliant `clone` methods.
+implementations are guaranteed not to mutate the `Cell` content through the `&self`
+reference. This is enforced with the provided `PureClone` trait, which is a subtrait
+of `Clone` (and a logical supertrait of `Copy`). It is only implemented for types
+with compliant `clone` methods.
 
 ## Overview
 
 The `Cell` implementation provided by this crate is intended to be a drop-in
-replacement of `std::cell::Cell`. It can work with types that behave like "values",
-such as `Rc<T>`, `Weak<T>` (shared pointers themselves are like values; it is the
-pointees that behave like references), `Option<T: PureClone>`, and more. Some
-motivating use cases include combining `Cell` with clone-on-write or immutable
-collections to enable efficient mutable sharing of data structures.
+replacement of `std::cell::Cell`. It can work with types that behave like *values*,
+such as `Rc<T>`, `Weak<T>` (shared pointers themselves are like *values*; it is the
+pointees that behave like *references*), `Option<T: PureClone>`, and more. Some
+motivating use cases include implementing the observer pattern and combining `Cell`
+with clone-on-write or immutable collections to enable efficient sharing of data
+structures.
 
 `PureClone` is currently implemented for the following types:
 - All primitives such as `i32`, `usize`, `f64`, etc;
@@ -43,7 +44,7 @@ See the documentation for [`Cell`] for more.
 ## Limitations
 
 - Similar to `std::cell::Cell`, this `Cell` is `!Sync`.
-- `PureClone` is currently only implemented for some types from the standard library. I hope to support user types as well with a proc macro so that we can automatically derive `PureClone` for types when they only contain fields that are `PureClone`:
+- `PureClone` is currently only implemented for some types from the standard library. I hope to support user types as well with a proc macro so that one can automatically derive `PureClone` for types when they only contain fields that are `PureClone`:
    ```rust
    // Not supported yet!
    #[derive(PureClone, Clone)]
--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,42 @@
 //! This crate provides a [`Cell`] implementation that works with types whose `Clone`
-//! implementations are guaranteed not to mutate the `Cell` content through the
-//! `self` reference the `clone` method gets. This is done with the provided
-//! [`PureClone`] trait, which is a subtrait of [`Clone`] (and a logical supertrait
-//! of [`Copy`]). It is only implemented for types with compliant `clone` methods.
+//! implementations are guaranteed not to mutate the `Cell` content using the `&self`
+//! reference. This is enforced with the provided [`PureClone`] trait, which is a
+//! subtrait of [`Clone`] (and a logical supertrait of [`Copy`]). It is only
+//! implemented for types with compliant `clone` methods.
+//!
+//! See the [`cell` module] documentation for more information on how to use it.
 //!
 //! # Background
 //!
-//! This crate was largely inspired by the Swift programming language's
-//! class properties (fields in Rust speak), which have value semantics. In Swift,
-//! class types themselves have reference semantics and are shared. But methods on
-//! class types are mutating. The Swift compiler is able to guarantee memory safety
-//! in a single-threaded context because copy constructors are not defined by the
-//! user. Intead, the compiler automatically generates ones that simply perform a
-//! field-wise clone.
+//! This crate was largely inspired by the Swift programming language's class
+//! properties (fields in Rust speak), which have value semantics. In Swift, class
+//! types themselves have reference semantics and are shared. But methods on class
+//! types are mutating. My observation is that the Swift compiler is able to
+//! guarantee memory safety in a single-threaded context because copy constructors
+//! are not defined by the user. Intead, the compiler automatically generates ones
+//! that simply perform a field-wise clone.
 //!
-//! In Rust, to enable interior-mutating methods on a `struct` stored in an `Rc`
-//! without the overhead of a `RefCell`, we can wrap each of the fields in a
-//! `Cell`. But `Cell::get` is only implemented for types that are `Copy`. This is
-//! because if the `clone` method obtains a reference to the `Cell`'s interior, it
-//! may be able to mutate its state. This can cause undefined behavior, as demonstrated
-//! in this [Rust forum thread].
+//! In Rust, to enable interiorly mutating methods on a `struct` stored in an [`Rc`]
+//! without the overhead of a [`RefCell`], we can wrap each of the fields in a
+//! [`std::cell::Cell`]. But its [`get`] method is only implemented for types that
+//! are `Copy`. This is because if the `clone` method obtains a reference to the
+//! `Cell`'s interior, it may be able to mutate its state. This can cause undefined
+//! behavior, as demonstrated in this [example].
 //!
-//! By restricting outselves to a checked subset of `Clone` implementations that do
+//! By restricting ourselves to a checked subset of `Clone` implementations that do
 //! not exploit interior mutability to mutate the `Cell` content, it becomes possible
 //! to provide a `Cell` with a `get` method that does not require `Copy` types.
-//!
-//! Note that just because there can be any number of readers and writers to a
-//! [`Cell`] does not mean it is a good idea. In most cases, it may not make sense to
-//! have more than one writer at a time. But the user can easily build abstractions
-//! on top of a `Cell` to enforce this. I think this may be useful when implementing
-//! the observer pattern.
 //!
 //! See the documentation for [`PureClone`] for a list of implemented types and more
 //! details.
 //!
-//! [`Cell`]: cell/struct.Cell.html
-//! [`Clone`]: https://doc.rust-lang.org/std/clone/trait.Clone.html
-//! [`Copy`]: https://doc.rust-lang.org/std/marker/trait.Copy.html
-//! [`PureClone`]: clone/trait.PureClone.html
-//! [Rust forum thread]:
+//! [`Cell`]: cell::Cell
+//! [`PureClone`]: clone::PureClone
+//! [`cell` module]: cell
+//! [`Rc`]: std::rc::Rc
+//! [`RefCell`]: std::cell::RefCell
+//! [`get`]: std::cell::Cell::get
+//! [example]:
 //! https://users.rust-lang.org/t/why-does-cell-require-copy-instead-of-clone/5769/3
 
 // TODO: #![no_std]
